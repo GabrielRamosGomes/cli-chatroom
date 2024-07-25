26773db6d1f84a9fe1cca7823174d101dc0620ff
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2024-07-25 16:02:55 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,11 +1,12 @@
 import unittest
 import socket
 import threading
+import os
 import time
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from server import Server
-from database import Base, User, Room, Message, PrivateMessage, db, user_room_table
+from database import Database
 
 def wait_for_condition(check_function, timeout=5, interval=0.1):
     start_time = time.time()
@@ -15,25 +16,34 @@ def wait_for_condition(check_function, timeout=5, interval=0.1):
         time.sleep(interval)
     return False
 
-
+DATABASE_PATH = 'test.db'
 class TestServer(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.engine = create_engine('sqlite:///:memory:')
-        Base.metadata.create_all(cls.engine)
-        cls.Session = sessionmaker(bind=cls.engine)
+        cls.db = Database(f'sqlite:///{DATABASE_PATH}')
+        cls.session = cls.db.get_session()
 
-        cls.session = cls.Session()
         cls.server = Server(session=cls.session)
-
         cls.server_thread = threading.Thread(target=cls.server.start, daemon=True)
         cls.server_thread.start()
-        wait_for_condition(lambda: cls.server.server.fileno() != -1)
+        
+        wait_for_condition(lambda: cls.server.server.fileno() != -1) 
+    
+    @classmethod
+    def tearDownClass(cls):
+        cls.server.server.close()
+        cls.server_thread.join()
+
+        cls.session.close()
+        cls.db.engine.dispose()
+
+        if os.path.exists(DATABASE_PATH):
+            os.remove(DATABASE_PATH)
 
     def setUp(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect(('127.0.0.1', 12345))
-        self.client.recv(1024)
+        self.client.recv(1024)  # Read initial prompt
 
     def tearDown(self):
         self.client.close()
